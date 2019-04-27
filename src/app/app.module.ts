1d0248243c8e1f2cd19ac910df3cddd7ded2eda2
Oleh Kolinko <okolinko@jetsmarter.com>
2019-04-27 13:47:33 -0400

    polyfills

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 
 const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
+(window as any).global = window;
 
 @NgModule({
   declarations: [
